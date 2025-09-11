10a5956ee9020ba1d1572c1e7e93443e854d55c8
kitmtaewoo <bezzi1654@kookmin.ac.kr>
2025-09-11 20:10:44 +0900

    feat: 최종 수정 완료

--- src/App.js
+++ src/App.js
@@ -1,26 +1,27 @@
 // src/App.js
-import React from 'react';
-import { BrowserRouter, Routes, Route } from 'react-router-dom';
-import { QueryClient, QueryClientProvider } from '@tanstack/react-query';
-import Header from './components/Header';
-import Footer from './components/Footer';
-import HomePage from './pages/HomePage';
-import ArticleListPage from './pages/ArticleListPage';
-import ArticleDetailPage from './pages/ArticleDetailPage';
-import ArticleFormPage from './pages/ArticleFormPage';
-import LoginPage from './pages/LoginPage';
-import RegisterPage from './pages/RegisterPage';
-import NotFoundPage from './pages/NotFoundPage';
+import React from "react";
+import { BrowserRouter, Routes, Route } from "react-router-dom";
+import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
+import Header from "./components/Header";
+import Footer from "./components/Footer";
+import HomePage from "./pages/HomePage";
+import ArticleListPage from "./pages/ArticleListPage";
+import ArticleDetailPage from "./pages/ArticleDetailPage";
+import ArticleFormPage from "./pages/ArticleFormPage";
+import LoginPage from "./pages/LoginPage";
+import RegisterPage from "./pages/RegisterPage";
+import NotFoundPage from "./pages/NotFoundPage";
 
 const queryClient = new QueryClient({
   defaultOptions: {
     queries: {
       refetchOnWindowFocus: false,
-      retry: 1
-    }
-  }
+      retry: 1,
+    },
+  },
 });
 
+//
 function App() {
   return (
     <QueryClientProvider client={queryClient}>
